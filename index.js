c122dd157b402f387f2ad3fd290fffe9c26aee72
Abdoul <abdoulramanediallo44@gmail.com>
2023-06-05 21:12:13 +0000

    Add a dynamic main js for the import module

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import Book from './modules/Book.js';
-import { showTime } from './modules/showTime.js';
+import showTime from './modules/showTime.js';
 import navLinks from './modules/showNavigation.js';
 import addNew from './modules/addNew.js';
 
-
 window.onload = () => {
-    Book.showBooks();
-    showTime();
-    document.querySelector('form').addEventListener('submit', addNew);
-    navLinks()
-  };
+  Book.showBooks();
+  showTime();
+  document.querySelector('form').addEventListener('submit', addNew);
+  navLinks();
+};
